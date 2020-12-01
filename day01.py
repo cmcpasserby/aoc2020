4dd0e948b77926d89f5dac68a6cc7eb093fb0489
Chris Cunningham <chris@rightsomegoodgames.ca>
2020-12-01 09:13:03 -0400

    made things a little slimmer, no need for a main function

--- day01.py
+++ day01.py
@@ -3,16 +3,11 @@ from itertools import combinations
 from math import prod
 
 
-def get_answer(data: List[int], r: int):
-    return next(prod(d) for d in combinations(data, r) if sum(d) == 2020)
+def get_answer(d: List[int], r: int):
+    return next(prod(d) for d in combinations(d, r) if sum(d) == 2020)
 
 
-def main():
-    with open("inputs/day01.txt", 'r') as f:
-        data = [int(i) for i in f.readlines()]
-        print(f"part a: {get_answer(data, 2)}")
-        print(f"part b: {get_answer(data, 3)}")
-
-
-if __name__ == '__main__':
-    main()
+with open("inputs/day01.txt", 'r') as f:
+    data = [int(i) for i in f.readlines()]
+    print(f"part a: {get_answer(data, 2)}")
+    print(f"part b: {get_answer(data, 3)}")
