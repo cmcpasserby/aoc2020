a37537b75310404a4aa554b0913a11c456ca5f38
Chris Cunningham <chris@rightsomegoodgames.ca>
2020-12-12 20:37:04 -0400

    day 10 part 2 solution

--- day10.py
+++ day10.py
@@ -1,22 +1,38 @@
-from itertools import tee
 from collections import Counter
-from typing import Iterator, Iterable
+from itertools import tee, islice
+from typing import Iterator, Iterable, TypeVar
+
+T = TypeVar('T')
 
 
-def pairs(iterable: Iterable[int]) -> Iterator[tuple[int, int]]:
-    a, b = tee(iterable, 2)
-    next(b)
-    return zip(a, b)
+def grouper(iterable: Iterable[T], n: int) -> Iterator[tuple[T, ...]]:
+    slices = (islice(it, i, None) for i, it in enumerate(tee(iterable, n)))
+    return zip(*slices)
 
 
 with open("inputs/day10.txt", 'r') as f:
     adapters = sorted(int(line) for line in f.read().splitlines())
-    adapters.append(adapters[-1] + 3)
+    adapters.append(adapters[-1] + 3)  # pc
 
     def part_a() -> int:
-        diffs = Counter(b - a for a, b in pairs(adapters))
+        diffs = Counter(b - a for a, b in grouper(adapters, 2))
         diffs[adapters[0]] += 1  # wall
         return diffs[1] * diffs[3]
 
-    print(f"part a: {part_a()}")
+    def part_b() -> int:
+        sums = 0
+        paths = [0] * len(adapters)
 
+        ptr = 0
+        for i, jolts in enumerate(adapters):
+            while ptr < i and adapters[ptr] < jolts-3:
+                sums -= paths[ptr]
+                ptr += 1
+
+            paths[i] = sums + (jolts <= 3)
+            sums += paths[i]
+
+        return paths[-1]
+
+    print(f"part a: {part_a()}")
+    print(f"part b: {part_b()}")
