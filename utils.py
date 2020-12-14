fde1289522aafbb43e73d44079a0db13b3cce6b1
Chris Cunningham <chris@rightsomegoodgames.ca>
2020-12-14 00:41:29 -0400

    added helper to test if number is prime

--- utils.py
+++ utils.py
@@ -15,3 +15,14 @@ def timer(count: int = 1):
             return ret
         return wrapper
     return decorator
+
+
+def is_prime(x: int) -> bool:
+    if x <= 1:
+        return False
+
+    for i in range(2, x):
+        if x % i == 0:
+            return False
+
+    return True
