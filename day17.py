a9032ee1c11a39879b5991336e8a22fb36089bf8
Chris Cunningham <chris@rightsomegoodgames.ca>
2020-12-17 22:00:33 -0400

    some day 17 cleanup

--- day17.py
+++ day17.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 from itertools import product
 from typing import NamedTuple, Iterable
 
+from utils import timer
 
 _TEST_DATA = """.#.
 ..#
@@ -37,15 +38,16 @@ def get_neighbours(start_vector: Vector, dimensions: int) -> Iterable[Vector]:
     return (start_vector + Vector(*d) for d in product(range(-1, 2), repeat=dimensions) if any(d))
 
 
+@timer(1)
 def solve(dimensions: int) -> int:
     grid = parse_grid()
 
     for _ in range(6):
+        new_grid = defaultdict(bool)
+
         # expand grid out around active
         for vec in [k for k, v in grid.items() if v]:
-            grid |= {i: True for i in get_neighbours(vec, dimensions) if grid[i]}
-
-        new_grid = grid.copy()
+            new_grid |= {i: grid[i] for i in get_neighbours(vec, dimensions)}
 
         for vec, value in new_grid.items():
             active_count = sum(grid[i] for i in get_neighbours(vec, dimensions))
